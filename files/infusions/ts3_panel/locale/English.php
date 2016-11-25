043487d59816209e5065459283fdf49e212ecfc1
Septron <support@septron.de>
2016-11-25 14:45:47 +0100

    Add files via upload

---
+++ files/infusions/ts3_panel/locale/English.php
@@ -0,0 +1,180 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright © 2002 - 2016 Nick Jones
+| http://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Name: SGI Fusion TS3 Viewer (für v7.02.xx)
+| Filename: English.php
+| Authors: Septron, Harlekin, PlanetTeamspeak, ts3admin
+| Support Website: http://www.septron.de
+|				   http://www.septron.eu
+|				   http://www.septron.net
+|				   http://www.septron.org
+|				   http://www.septron.info
+|				   http://phpfusion-deutschland.de
+|				   http://harlekinpower.de
+|				   https://www.planetteamspeak.com
+|				   http://ts3admin.info
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['ts3over000'] = "Server Status";
+$locale['ts3over001'] = "Online";
+$locale['ts3over002'] = "Server Name";
+$locale['ts3over003'] = "Server IP";
+$locale['ts3over004'] = "Created on";
+$locale['ts3over005'] = "Server Uptime";
+$locale['ts3over006'] = "System";
+$locale['ts3over007'] = "Total Slots";
+$locale['ts3over008'] = "Total Channels";
+$locale['ts3over009'] = "Teamspeak&sup3; Version";
+$locale['ts3over010'] = "&bull;&gt;&gt; VOICE &lt;&lt;&bull;";
+$locale['ts3over011'] = "Download";
+$locale['ts3over012'] = "Upload";
+$locale['ts3over013'] = "&bull;&gt;&gt; FILE &lt;&lt;&bull;";
+$locale['ts3over014'] = "&bull;&gt;&gt; TOTAL &lt;&lt;&bull;";
+$locale['ts3over015'] = "Server Status: OFFLINE";
+$locale['ts3over016'] = "<b>ID</b>";
+$locale['ts3over017'] = "<b>Servername</b>";
+$locale['ts3over018'] = "<b>Port</b>";
+$locale['ts3over019'] = "<b>Clients</b>:&nbsp;";
+$locale['ts3over020'] = "<b>Status</b>";
+$locale['ts3over021'] = "<b>Uptime</b>";
+$locale['ts3over022'] = "Teamspeak 3 Viewer";
+$locale['ts3over023'] = "List of our servers";
+$locale['ts3over024'] = "used";
+$locale['ts3over025'] = "Host:";
+$locale['ts3over026'] = "... The TS3 server hostname, or the server IP ...";
+$locale['ts3over027'] = "Query Port:";
+$locale['ts3over028'] = "... The TS3 Query Port, for example 10011 ...";
+$locale['ts3over029'] = "Port:";
+$locale['ts3over030'] = "... The TS3 port, for example 9987 ...";
+$locale['ts3over031'] = "Server ID:";
+$locale['ts3over032'] = "... The TS3 Server ID ...";
+$locale['ts3over033'] = "Timeout:";
+$locale['ts3over034'] = "... Timeout of all actions in seconds ...";
+$locale['ts3over035'] = "Admin Name:";
+$locale['ts3over036'] = "... The ServerQuery Adminloginname (For the server list) ...";
+$locale['ts3over037'] = "Admin Passwort:";
+$locale['ts3over038'] = "... the ServerQuery Adminpassword (For the server list) ...";
+$locale['ts3over039'] = "false";
+$locale['ts3over040'] = "true";
+$locale['ts3over041'] = "Empty channel:";
+$locale['ts3over042'] = "hide";
+$locale['ts3over043'] = "display";
+$locale['ts3over044'] = "... Should empty channels be displayed? ...";
+$locale['ts3over045'] = "Parent Channel:";
+$locale['ts3over046'] = "... Should the parent channel be displayed? ...";
+$locale['ts3over047'] = "Yours";
+$locale['ts3over048'] = "questioned";
+$locale['ts3over049'] = "Version";
+$locale['ts3over050'] = "Status";
+$locale['ts3over051'] = "Empty channel in Panel:";
+$locale['ts3over052'] = "... Should empty channels be displayed in site panel? ...";
+$locale['ts3over053'] = "Parent channel in Panel:";
+$locale['ts3over054'] = "... Should the parent channel be displayed in site panel? ...";
+$locale['ts3over055'] = "server list:";
+$locale['ts3over056'] = "... Should the server list be displayed in the page link? ...";
+$locale['ts3over057'] = "Update";
+$locale['ts3over058'] = "<center><b>Server is currently offline</b></center>";
+$locale['ts3over059'] = "Teamspeak&sup3; Settings";
+$locale['ts3over060'] = "Button in the panel:";
+$locale['ts3over061'] = "... Should the join button be displayed in the panel? ...";
+$locale['ts3over062'] = "Button in the page link:";
+$locale['ts3over063'] = "... Should the join button be displayed in the page link? ...";
+$locale['ts3over064'] = "Server list in the page link:";
+$locale['ts3over065'] = "... Should the server list be displayed in the page link? ...";
+$locale['ts3over066'] = "Serverbanner in the page link:";
+$locale['ts3over067'] = "... Should the server banner be displayed in the page link? ...";
+$locale['ts3over068'] = "Welcome to the site link:";
+$locale['ts3over069'] = "... Is the welcome message displayed in the page link? ...";
+$locale['ts3over070'] = "Servername in the page link:";
+$locale['ts3over071'] = "... Should the server name be displayed in the page link? ...";
+$locale['ts3over072'] = "Server IP in the page link:";
+$locale['ts3over073'] = "... Should the server IP be displayed in the page link? ...";
+$locale['ts3over074'] = "Server port in the page link:";
+$locale['ts3over075'] = "... Should the server port be displayed in the page link? ...";
+$locale['ts3over076'] = "Server version in the page link:";
+$locale['ts3over077'] = "... Should the server version be displayed in the page link? ...";
+$locale['ts3over078'] = "Server platform in the page link:";
+$locale['ts3over079'] = "... Should the server platform be displayed in the page link? ...";
+$locale['ts3over080'] = "Server status in the page link:";
+$locale['ts3over081'] = "... Should the server status displayed in the page link? ...";
+$locale['ts3over082'] = "Server Create in the page link:";
+$locale['ts3over083'] = "... Do you want to display the Create servers in the page link? ...";
+$locale['ts3over084'] = "Server Online in the page link:";
+$locale['ts3over085'] = "... Should the server online time be displayed in the page link? ...";
+$locale['ts3over086'] = "Server start in the page link:";
+$locale['ts3over087'] = "... Do you want to display the last server reboot in the page link? ...";
+$locale['ts3over088'] = "Server users in the page link:";
+$locale['ts3over089'] = "... Should the server users be displayed in the page link? ...";
+$locale['ts3over090'] = "Server Channel in the page link:";
+$locale['ts3over091'] = "... Should the number of server channels displayed in the page link? ...";
+$locale['ts3over092'] = "Server Voice in the page link:";
+$locale['ts3over093'] = "... Should the server voice data exchange be displayed in the page link? ...";
+$locale['ts3over094'] = "Server file in the page link:";
+$locale['ts3over095'] = "... Should the server data exchange be displayed in the page link? ...";
+$locale['ts3over096'] = "Join password:";
+$locale['ts3over097'] = "... Join Enter password when one is used ...";
+$locale['ts3over098'] = "Join password in the page link:";
+$locale['ts3over099'] = "... Should the join password to be displayed in the page link if one exists? ...";
+$locale['ts3over100'] = "Server Smartphone in the page link:";
+$locale['ts3over101'] = "... Should the server smartphone users be displayed in the page link? ...";
+$locale['ts3over102'] = "--- selection ---";
+$locale['ts3over103'] = "Server Port";
+$locale['ts3over104'] = "server created";
+$locale['ts3over105'] = "Server since";
+$locale['ts3over106'] = "Server last started before";
+$locale['ts3over107'] = "Server Total exchange in the page link:";
+$locale['ts3over108'] = "... Do you want to display the server's total exchange in the page link? ...";
+$locale['ts3over109'] = "Day/s";
+$locale['ts3over110'] = "Month transfer in the page link:";
+$locale['ts3over111'] = "... Should the monthly data transfer to be displayed in the page link? ...";
+$locale['ts3over112'] = "&bull;&gt;&gt; Month &lt;&lt;&bull;";
+$locale['ts3over113'] = "Required safety level";
+$locale['ts3over114'] = "Security level in the page link:";
+$locale['ts3over115'] = "... Should the security level to be displayed in the page link? ...";
+$locale['ts3over116'] = "&bull;&gt;&gt; packages &lt;&lt;&bull;";
+$locale['ts3over117'] = "Packages in the page link:";
+$locale['ts3over118'] = "... Should the package data transfer to be displayed in the page link? ...";
+$locale['ts3over119'] = "Server Ping";
+$locale['ts3over120'] = "Server Ping in the page link:";
+$locale['ts3over121'] = "... Should the server ping be displayed in the page link? ...";
+
+$locale['ts3overver_000'] = "Version check";
+$locale['ts3overver_001'] = "Update check: There is an Update available!!";
+$locale['ts3overver_002'] = "<strong>Current version</strong> ";
+$locale['ts3overver_003'] = "<strong>New version</strong> ";
+$locale['ts3overver_004'] = "You can download it <a href='https://www.septron.de/' target='_blank'>on this site (SGI Fusion)</a> - Or you can download it <a href='https://phpfusion-deutschland.de/' target='_blank'>on this site (PHPFusion Germany)</a>";
+$locale['ts3overver_005'] = "Update check: You have the latest version";
+$locale['ts3overver_006'] = "<strong>Version</strong> ";
+$locale['ts3overver_007'] = "There is no updates available.";
+$locale['ts3overver_008'] = "Version check failed";
+$locale['ts3overver_009'] = "The Version Check couldn't be executed. Please Report this to <a href='https://www.septron.de/' target='_blank'>SGI Fusion</a> or here <a href='https://phpfusion-deutschland.de/' target='_blank'>PHPFusion Deutschland</a>";
+$locale['ts3overver_010'] = "To check the version, \"allow_url_fopen\" hast to be set to ON in php.ini";
+$locale['ts3overver_011'] = "<strong>Version</strong> ";
+
+$locale['copyover_001'] = "&copy; by";
+$locale['copyover_002'] = "SGI Fusion";
+$locale['copyover_003'] = "PHPFusion Deutschland";
+$locale['copyover_004'] = "HarlekinPower";
+$locale['copyover_005'] = "PlanetTeamspeak";
+$locale['copyover_006'] = "ts3admin.info";
+$locale['copyover_007'] = "Published on Oct 16, 2016 - Last update:";
+$locale['copyover_008'] = "&nbsp;Clock";
+$locale['copyover_009'] = "https://www.septron.de";
+$locale['copyover_010'] = "https://phpfusion-deutschland.de";
+$locale['copyover_011'] = "http://harlekinpower.de";
+$locale['copyover_012'] = "https://www.planetteamspeak.com";
+$locale['copyover_013'] = "http://ts3admin.info";
+$locale['copyover_014'] = "around";
+$locale['copyover_haupt'] = "Copyright";
+$locale['copyover_zusatz'] = "&nbsp;|&nbsp;";
+?>
---
+++ files/infusions/ts3_panel/locale/German.php
@@ -0,0 +1,180 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright © 2002 - 2016 Nick Jones
+| http://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Name: SGI Fusion TS3 Viewer (für v7.02.xx)
+| Filename: German.php
+| Authors: Septron, Harlekin, PlanetTeamspeak, ts3admin
+| Support Website: http://www.septron.de
+|				   http://www.septron.eu
+|				   http://www.septron.net
+|				   http://www.septron.org
+|				   http://www.septron.info
+|				   http://phpfusion-deutschland.de
+|				   http://harlekinpower.de
+|				   https://www.planetteamspeak.com
+|				   http://ts3admin.info
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['ts3over000'] = "Server Status";
+$locale['ts3over001'] = "Online";
+$locale['ts3over002'] = "Server Name";
+$locale['ts3over003'] = "Server IP";
+$locale['ts3over004'] = "Erstellt am";
+$locale['ts3over005'] = "Server Laufzeit";
+$locale['ts3over006'] = "Plattform";
+$locale['ts3over007'] = "Benutzer platz";
+$locale['ts3over008'] = "Channel insgesamt";
+$locale['ts3over009'] = "Teamspeak&sup3; Version";
+$locale['ts3over010'] = "&bull;&gt;&gt; Sprache &lt;&lt;&bull;";
+$locale['ts3over011'] = "Heruntergeladen";
+$locale['ts3over012'] = "Hochgeladen";
+$locale['ts3over013'] = "&bull;&gt;&gt; Dateien &lt;&lt;&bull;";
+$locale['ts3over014'] = "&bull;&gt;&gt; Gesamt &lt;&lt;&bull;";
+$locale['ts3over015'] = "Server Status: OFFLINE";
+$locale['ts3over016'] = "<b>ID</b>";
+$locale['ts3over017'] = "<b>Servername</b>";
+$locale['ts3over018'] = "<b>Port</b>";
+$locale['ts3over019'] = "<b>Benutzer</b>:&nbsp;";
+$locale['ts3over020'] = "<b>Status</b>";
+$locale['ts3over021'] = "<b>Laufzeit</b>";
+$locale['ts3over022'] = "Teamspeak 3 Viewer";
+$locale['ts3over023'] = "Liste unserer Server";
+$locale['ts3over024'] = "benutzt";
+$locale['ts3over025'] = "Host:";
+$locale['ts3over026'] = "... der TS3 Server Hostname, bzw. die Server IP ...";
+$locale['ts3over027'] = "Query Port:";
+$locale['ts3over028'] = "... der TS3 Query Port, z.b. 10011 ...";
+$locale['ts3over029'] = "Port:";
+$locale['ts3over030'] = "... der TS3 Port, z.b. 9987 ...";
+$locale['ts3over031'] = "Server ID:";
+$locale['ts3over032'] = "... die TS3 Server ID ...";
+$locale['ts3over033'] = "Timeout:";
+$locale['ts3over034'] = "... Timeout aller Aktionen in Sekunden ...";
+$locale['ts3over035'] = "Admin Name:";
+$locale['ts3over036'] = "... der ServerQuery Adminloginname (Für die Serverliste) ...";
+$locale['ts3over037'] = "Admin Passwort:";
+$locale['ts3over038'] = "... das ServerQuery Adminpasswort (Für die Serverliste) ...";
+$locale['ts3over039'] = "false";
+$locale['ts3over040'] = "true";
+$locale['ts3over041'] = "Leere Channel:";
+$locale['ts3over042'] = "verstecken";
+$locale['ts3over043'] = "anzeigen";
+$locale['ts3over044'] = "... sollen Leere Channel angezeigt werden? ...";
+$locale['ts3over045'] = "&Uuml;bergeordnete Channel:";
+$locale['ts3over046'] = "... sollen &Uuml;bergeordnete Channel angezeigt werden? ...";
+$locale['ts3over047'] = "Deine";
+$locale['ts3over048'] = "Abgefragte";
+$locale['ts3over049'] = "Version";
+$locale['ts3over050'] = "Status";
+$locale['ts3over051'] = "Leere Channel im Panel:";
+$locale['ts3over052'] = "... sollen Leere Channel im Seitenpanel angezeigt werden? ...";
+$locale['ts3over053'] = "&Uuml;bergeordnete Channel im Panel:";
+$locale['ts3over054'] = "... sollen &Uuml;bergeordnete Channel im Seitenpanel angezeigt werden? ...";
+$locale['ts3over055'] = "Serverliste:";
+$locale['ts3over056'] = "... soll die Serverliste im Seitenlink angezeigt werden? ...";
+$locale['ts3over057'] = "Aktualisierung";
+$locale['ts3over058'] = "<center><b>Server ist derzeit Offline</b></center>";
+$locale['ts3over059'] = "Teamspeak 3 Einstellungen";
+$locale['ts3over060'] = "Button im Panel:";
+$locale['ts3over061'] = "... Soll der Join Button im Panel angezeigt werden? ...";
+$locale['ts3over062'] = "Button im Seitenlink:";
+$locale['ts3over063'] = "... Soll der Join Button im Seitenlink angezeigt werden? ...";
+$locale['ts3over064'] = "Serverliste im Seitenlink:";
+$locale['ts3over065'] = "... Soll die Serverliste im Seitenlink angezeigt werden? ...";
+$locale['ts3over066'] = "Server Banner im Seitenlink:";
+$locale['ts3over067'] = "... Soll der Serverbanner im Seitenlink angezeigt werden? ...";
+$locale['ts3over068'] = "Willkommen im Seitenlink:";
+$locale['ts3over069'] = "... Soll die Willkommensmeldung im Seitenlink angezeigt werden? ...";
+$locale['ts3over070'] = "Servername im Seitenlink:";
+$locale['ts3over071'] = "... Soll der Servername im Seitenlink angezeigt werden? ...";
+$locale['ts3over072'] = "Server IP im Seitenlink:";
+$locale['ts3over073'] = "... Soll die Server IP im Seitenlink angezeigt werden? ...";
+$locale['ts3over074'] = "Server Port im Seitenlink:";
+$locale['ts3over075'] = "... Soll der Server Port im Seitenlink angezeigt werden? ...";
+$locale['ts3over076'] = "Server Version im Seitenlink:";
+$locale['ts3over077'] = "... Soll die Server Version im Seitenlink angezeigt werden? ...";
+$locale['ts3over078'] = "Server Plattform im Seitenlink:";
+$locale['ts3over079'] = "... Soll die Server Plattform im Seitenlink angezeigt werden? ...";
+$locale['ts3over080'] = "Server Status im Seitenlink:";
+$locale['ts3over081'] = "... Soll der Server Status im Seitenlink angezeigt werden? ...";
+$locale['ts3over082'] = "Server Erstellung im Seitenlink:";
+$locale['ts3over083'] = "... Soll die Server Erstellung im Seitenlink angezeigt werden? ...";
+$locale['ts3over084'] = "Server Online im Seitenlink:";
+$locale['ts3over085'] = "... Soll die Server Online Zeit im Seitenlink angezeigt werden? ...";
+$locale['ts3over086'] = "Server Start im Seitenlink:";
+$locale['ts3over087'] = "... Soll der letzte Server Neustart im Seitenlink angezeigt werden? ...";
+$locale['ts3over088'] = "Server Benutzer im Seitenlink:";
+$locale['ts3over089'] = "... Sollen die Server Benutzer im Seitenlink angezeigt werden? ...";
+$locale['ts3over090'] = "Server Channel im Seitenlink:";
+$locale['ts3over091'] = "... Soll die anzahl der Server Channel im Seitenlink angezeigt werden? ...";
+$locale['ts3over092'] = "Server Voice im Seitenlink:";
+$locale['ts3over093'] = "... Soll der Server Voice Datenaustausch im Seitenlink angezeigt werden? ...";
+$locale['ts3over094'] = "Server File im Seitenlink:";
+$locale['ts3over095'] = "... Soll der Server Datenaustausch im Seitenlink angezeigt werden? ...";
+$locale['ts3over096'] = "Join Passwort:";
+$locale['ts3over097'] = "... Join Passwort eingeben wenn eins verwendet wird ...";
+$locale['ts3over098'] = "Join Paswort im Seitenlink:";
+$locale['ts3over099'] = "... Soll das Join Passwort im Seitenlink angezeigt werden falls eins vorhanden ist? ...";
+$locale['ts3over100'] = "Server Smartphone im Seitenlink:";
+$locale['ts3over101'] = "... Sollen die Server Smartphone Benutzer im Seitenlink angezeigt werden? ...";
+$locale['ts3over102'] = "--- Auswahl ---";
+$locale['ts3over103'] = "Server Port";
+$locale['ts3over104'] = "Server erstellt";
+$locale['ts3over105'] = "Server seit";
+$locale['ts3over106'] = "Server zuletzt gestartet vor";
+$locale['ts3over107'] = "Server Gesamt transfer im Seitenlink:";
+$locale['ts3over108'] = "... Soll der Server Gesamtaustausch im Seitenlink angezeigt werden? ...";
+$locale['ts3over109'] = "Tag/e";
+$locale['ts3over110'] = "Monats transfer im Seitenlink:";
+$locale['ts3over111'] = "... Soll der Monatlicher Datentransfer im Seitenlink angezeigt werden? ...";
+$locale['ts3over112'] = "&bull;&gt;&gt; Monat &lt;&lt;&bull;";
+$locale['ts3over113'] = "Ben&ouml;tigte Sicherheitsstufe";
+$locale['ts3over114'] = "Sicherheitsstufe im Seitenlink:";
+$locale['ts3over115'] = "... Soll die Sicherheitsstufe im Seitenlink angezeigt werden? ...";
+$locale['ts3over116'] = "&bull;&gt;&gt; Pakete &lt;&lt;&bull;";
+$locale['ts3over117'] = "Pakete im Seitenlink:";
+$locale['ts3over118'] = "... Soll der Paket Datentransfer im Seitenlink angezeigt werden? ...";
+$locale['ts3over119'] = "Server Ping";
+$locale['ts3over120'] = "Server Ping im Seitenlink:";
+$locale['ts3over121'] = "... Soll der Server Ping im Seitenlink angezeigt werden? ...";
+
+$locale['ts3overver_000'] = "Version &uuml;berpr&uuml;fen";
+$locale['ts3overver_001'] = "Update-&Uuml;berpr&uuml;fung: Es ist ein Update verf&uuml;gbar!";
+$locale['ts3overver_002'] = "<strong>Aktuelle Version</strong> ";
+$locale['ts3overver_003'] = "<strong>Neue Version</strong> ";
+$locale['ts3overver_004'] = "Du kannst es auf dieser Seite herunterladen: <a href='https://www.septron.de/' target='_blank'>SGI Fusion Besuchen</a> - Oder auf dieser Seite herunterladen: <a href='https://phpfusion-deutschland.de/' target='_blank'>PHPFusion Deutschland Besuchen</a>";
+$locale['ts3overver_005'] = "Update-&Uuml;berpr&uuml;fung: Du hast die neueste Version";
+$locale['ts3overver_006'] = "<strong>Version</strong> ";
+$locale['ts3overver_007'] = "Es sind keine Updates verf&uuml;gbar.";
+$locale['ts3overver_008'] = "Versionspr&uuml;fung fehlgeschlagen";
+$locale['ts3overver_009'] = "Die Versionspr&uuml;fung konnte nicht ausgef&uuml;hrt werden. Bitte melden dies an <a href='https://www.septron.de/' target='_blank'>SGI Fusion</a> oder an <a href='https://phpfusion-deutschland.de/' target='_blank'>PHPFusion Deutschland</a>";
+$locale['ts3overver_010'] = "Um die Version zu &uuml;berpr&uuml;fen, muss \"allow_url_fopen\" in der php.ini auf AN gesetzt sein (wenn du nur einen Webserver hast musst du dies deinem Hoster &uuml;bermitteln).";
+$locale['ts3overver_011'] = "<strong>Version</strong> ";
+
+$locale['copyover_001'] = "&copy; by";
+$locale['copyover_002'] = "SGI Fusion";
+$locale['copyover_003'] = "PHPFusion Deutschland";
+$locale['copyover_004'] = "HarlekinPower";
+$locale['copyover_005'] = "PlanetTeamspeak";
+$locale['copyover_006'] = "ts3admin.info";
+$locale['copyover_007'] = "Veröffentlicht am 16.10.2016 - Letzte Aktualisierung:";
+$locale['copyover_008'] = "&nbsp;Uhr";
+$locale['copyover_009'] = "https://www.septron.de";
+$locale['copyover_010'] = "https://phpfusion-deutschland.de";
+$locale['copyover_011'] = "http://harlekinpower.de";
+$locale['copyover_012'] = "https://www.planetteamspeak.com";
+$locale['copyover_013'] = "http://ts3admin.info";
+$locale['copyover_014'] = "um";
+$locale['copyover_haupt'] = "Copyright";
+$locale['copyover_zusatz'] = "&nbsp;|&nbsp;";
+?>
